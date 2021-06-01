947dd3b683ca6f61928507e7917cbe47eedd8dde
cvicens <carlos.vicens.alonso@gmail.com>
2021-06-01 15:48:00 +0200

    datagrid cache for alerts

--- 12-quarkus-dashboard/src/main/java/com/redhat/banking/eda/dashboard/infrastructure/AlertProcessor.java
+++ 12-quarkus-dashboard/src/main/java/com/redhat/banking/eda/dashboard/infrastructure/AlertProcessor.java
@@ -1,11 +1,15 @@
 package com.redhat.banking.eda.dashboard.infrastructure;
 
+import io.quarkus.infinispan.client.Remote;
 import io.smallrye.reactive.messaging.annotations.Broadcast;
 import org.eclipse.microprofile.reactive.messaging.Acknowledgment;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 import com.redhat.banking.eda.dashboard.valueobjects.Alert;
 
@@ -16,11 +20,23 @@ import com.redhat.banking.eda.dashboard.valueobjects.Alert;
 @ApplicationScoped
 public class AlertProcessor {
 
+    @Inject 
+    @Remote("alerts")
+    RemoteCache<String, Alert> cache;
+
+    RemoteCacheManager remoteCacheManager;
+    
+    @Inject 
+    AlertProcessor(RemoteCacheManager remoteCacheManager) {
+        this.remoteCacheManager = remoteCacheManager;
+    }
+
     @Incoming("alerts")                                     
     @Outgoing("alerts-stream")                             
     @Broadcast                                              
     @Acknowledgment(Acknowledgment.Strategy.PRE_PROCESSING) 
     public Alert process(Alert alert) {
+        cache.put(alert.getId(), alert);
         return alert;
     }
 
