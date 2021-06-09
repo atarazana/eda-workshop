111011d04ba16991ac8949de2e4e446b58a94e91
Roman Martin <rmarting@redhat.com>
2021-06-09 07:10:40 +0200

    :zap: Sorting aggregated metrics

--- 12-quarkus-dashboard/src/main/java/com/redhat/banking/eda/dashboard/AggregateMetricResource.java
+++ 12-quarkus-dashboard/src/main/java/com/redhat/banking/eda/dashboard/AggregateMetricResource.java
@@ -1,22 +1,7 @@
 package com.redhat.banking.eda.dashboard;
 
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-
-import javax.inject.Inject;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-
 import com.redhat.banking.eda.dashboard.valueobjects.AggregateMetric;
-
+import io.quarkus.infinispan.client.Remote;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
@@ -26,35 +11,47 @@ import org.infinispan.query.dsl.QueryFactory;
 import org.infinispan.query.dsl.QueryResult;
 import org.jboss.resteasy.annotations.SseElementType;
 import org.reactivestreams.Publisher;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.quarkus.infinispan.client.Remote;
+import javax.inject.Inject;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * A simple resource retrieving the in-memory "my-data-stream" and sending the items as server-sent events.
  */
 @Path("/aggregate-metrics")
 public class AggregateMetricResource {
+
     private static final Logger LOG = LoggerFactory.getLogger(AggregateMetricResource.class);
 
     @Inject
-    @Channel("aggregate-metrics-stream") Publisher<AggregateMetric> metrics; 
+    @Channel("aggregate-metrics-stream")
+    Publisher<AggregateMetric> metrics;
 
-    @Inject 
+    @Inject
     @Remote("aggregate-metrics")
     RemoteCache<String, AggregateMetric> cache;
 
     RemoteCacheManager remoteCacheManager;
-    
-    @Inject 
+
+    @Inject
     AggregateMetricResource(RemoteCacheManager remoteCacheManager) {
         this.remoteCacheManager = remoteCacheManager;
     }
 
     @GET
-    public List<AggregateMetric> allAggregateMetrics() { 
+    public List<AggregateMetric> allAggregateMetrics() {
         LOG.info("allAggregateMetrics");
 
         ArrayList<AggregateMetric> aggregateMetrics = new ArrayList<AggregateMetric>();
@@ -64,33 +61,44 @@ public class AggregateMetricResource {
 
         LOG.info("size = " + cache.keySet().size());
 
-        return aggregateMetrics;  
+        return aggregateMetrics;
     }
 
     @GET
     @Path("/by-name")
-    public List<AggregateMetric> aggregateMetricsByName(@QueryParam("name") String name,  @QueryParam("groupByClause") String groupByClause, @DefaultValue("30") @QueryParam("period") int period) { 
+    public List<AggregateMetric> aggregateMetricsByName(@QueryParam("name") String name,
+                                                        @QueryParam("groupByClause") String groupByClause,
+                                                        @DefaultValue("30") @QueryParam("period") int period) {
         LOG.info("aggregateMetricsByName");
 
         QueryFactory queryFactory = Search.getQueryFactory(cache);
         String groupByClauseFull = groupByClause != null ? "AND groupByClause = '" + groupByClause + "'" : "";
-        Query<AggregateMetric> query = queryFactory.create("FROM eda.workshop.AggregateMetric WHERE name = '" + name + "' " + groupByClauseFull + " ORDER BY timestamp");
+        Query<AggregateMetric> query = queryFactory.create(
+                "FROM eda.workshop.AggregateMetric WHERE name = '" + name + "' " +
+                        groupByClauseFull +
+                        " ORDER BY timestamp");
 
         // Execute the query
         QueryResult<AggregateMetric> queryResult = query.execute();
 
         LOG.info("size = " + queryResult.hitCount());
 
-        return queryResult.list().stream().filter(c -> c.getTimestamp().compareTo(Instant.now().minusSeconds(period)) > 0 ).collect(Collectors.toList());
+        return queryResult.list()
+                .stream()
+                .filter(c -> c.getTimestamp().compareTo(Instant.now().minusSeconds(period)) > 0)
+                .collect(Collectors.toList());
     }
 
     @GET
     @Path("/by-name-region")
-    public List<AggregateMetric> aggregateMetricsByNameAndRegion(@QueryParam("name") String name, @QueryParam("region") String region) { 
+    public List<AggregateMetric> aggregateMetricsByNameAndRegion(@QueryParam("name") String name,
+                                                                 @QueryParam("region") String region) {
         LOG.info("aggregateMetricsByName");
 
         QueryFactory queryFactory = Search.getQueryFactory(cache);
-        Query<AggregateMetric> query = queryFactory.create("FROM eda.workshop.AggregateMetric WHERE name = '" + name + "' and from = '" + region + "' ORDER BY timestamp");
+        Query<AggregateMetric> query = queryFactory.create(
+                "FROM eda.workshop.AggregateMetric WHERE name = '" + name + "' and from = '" + region
+                        + "' ORDER BY timestamp");
 
         // Execute the query
         QueryResult<AggregateMetric> queryResult = query.execute();
@@ -102,9 +110,10 @@ public class AggregateMetricResource {
 
     @GET
     @Path("/stream")
-    @Produces(MediaType.SERVER_SENT_EVENTS) 
-    @SseElementType(MediaType.APPLICATION_JSON) 
-    public Publisher<AggregateMetric> stream() { 
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    @SseElementType(MediaType.APPLICATION_JSON)
+    public Publisher<AggregateMetric> stream() {
         return metrics;
     }
+
 }
